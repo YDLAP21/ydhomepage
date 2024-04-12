eb10a77ccefc6b86ea5e395e30987dfc7594d6c3
YDLAP21 <166393983+YDLAP21@users.noreply.github.com>
2024-04-12 17:18:13 +0900

    yd homepage v1
    yd homepage v1

---
+++ start.py
@@ -0,0 +1,15 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/hello")  #127.0.0.1
+def hello_world():
+    return "<p>Welcome to YD CBT Center!</p>"
+
+@app.route("/")  #127.0.0.1
+def index():
+    return send_from_directory('html', "index.html")  #127.0.0.1/html/css/b.css
+
+@app.route('/<path:name>')   #127.0.0.1/css/b.css
+def start(name):
+    return send_from_directory('html', name)  #127.0.0.1/html/css/b.css
